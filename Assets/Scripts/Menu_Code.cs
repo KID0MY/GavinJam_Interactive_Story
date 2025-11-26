7c0941994d8cbdcd1ce410c8fa3a2b9d7eb4d781
Lucas Triboli Bujes <57596595+KID0MY@users.noreply.github.com>
2025-11-25 20:08:33 -0500

    added a main menu and pause menu

---
+++ Assets/Scripts/Menu_Code.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Code : MonoBehaviour
+{
+    bool paused = false;
+
+    public GameObject Menu;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void pauseMenu()
+    {
+        paused = !paused;
+        Time.timeScale = paused ? 0 : 1;
+        Menu.SetActive(paused);
+    }
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MAIN_GameScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
